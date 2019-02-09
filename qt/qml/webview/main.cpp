38e1f1d8fd6d8afbe1af937b32a96e90acccc017
Gaël PORTAY <gael.portay@collabora.com>
2019-02-08 20:28:34 -0500

    qt: qml: fix warning message
    By calling QtWebView::initialize() method[1].
    
    Fixes:
    
    	WebEngineContext used before QtWebEngine::initialize() or OpenGL context creation failed.
    
    [1]: https://doc.qt.io/qt-5/qtwebview.html#initialize

--- qt/qml/webview/main.cpp
+++ qt/qml/webview/main.cpp
@@ -1,11 +1,14 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QtWebView>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+
+    QtWebView::initialize();
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
